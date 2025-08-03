0da67e38db19336ee05314b05b9a07466f5f2a3c
takamura-rk <20235865@etud.univ-evry.fr>
2025-08-03 21:20:08 +0200

    Calcul de déterminant de matrice

--- Determinant.py
+++ Determinant.py
@@ -1,11 +1,35 @@
-import numpy as np 
-import math 
-def det_mat(A,n):
-    n=len(A)
-    det=A[0][0]
-    for i in range (n):
-        det=det*A[i][i]
+def determinant(mat):
+    n = len(mat)
+    # Copie de la matrice 
+    A = [row[:] for row in mat]
+    det = 1
+    swaps = 0
 
+    for i in range(n):
+        # Recherche d'un pivot non nul
+        if A[i][i] == 0:
+            for j in range(i + 1, n):
+                if A[j][i] != 0:
+                    A[i], A[j] = A[j], A[i]
+                    swaps += 1
+                    break
+            else:
+                return 0 
+        pivot = A[i][i]
+        for j in range(i + 1, n):
+            factor = A[j][i] / pivot
+            for k in range(i, n):
+                A[j][k] -= factor * A[i][k]
 
+    # Produit des éléments diagonaux
+    for i in range(n):
+        det *= A[i][i]
+    return -det if swaps % 2 == 1 else det
 
-    return 
+matrice = [
+    [2, 1, 3],
+    [1, 0, 2],
+    [4, 1, 8]
+]
+
+print("Déterminant :", determinant(matrice))
